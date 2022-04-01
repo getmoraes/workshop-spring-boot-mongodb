047a0da3910cc4d2c7cacfbac53ecbcddd815e19
Daniel Moraes <daniel_fery@hotmail.com>
2022-03-31 21:17:32 -0300

    Projeto criado

---
+++ src/main/java/com/treino/workshopmongo/WorkshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.treino.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoApplication.class, args);
+	}
+
+}
