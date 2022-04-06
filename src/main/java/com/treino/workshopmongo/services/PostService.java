ded23821ff13561a8395a2a7b347fa7abaaf59b3
Daniel Moraes <daniel_fery@hotmail.com>
2022-04-06 20:24:17 -0300

    Obtendo um post por id

---
+++ src/main/java/com/treino/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.treino.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.treino.workshopmongo.domain.Post;
+import com.treino.workshopmongo.repository.PostRepository;
+import com.treino.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	// Mecanismo de injeção de depêndencia automatica do Spring
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
