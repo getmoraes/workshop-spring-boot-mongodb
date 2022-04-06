858ee7ac6ea15c3ad3d2f01e9c2964afbc519769
Daniel Moraes <daniel_fery@hotmail.com>
2022-04-06 20:21:17 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/treino/workshopmongo/resources/UserResource.java
+++ src/main/java/com/treino/workshopmongo/resources/UserResource.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.treino.workshopmongo.DTO.UserDTO;
+import com.treino.workshopmongo.domain.Post;
 import com.treino.workshopmongo.domain.User;
 import com.treino.workshopmongo.services.UserService;
 
@@ -60,5 +61,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
